dad9aa63065b54727520d4cc47a9255417548c3f
NickMateus11 <mateus.nicholas@gmail.com>
2021-01-17 14:46:05 -0500

    search textbox and POST submit

--- flask_image_repo.py
+++ flask_image_repo.py
@@ -1,30 +1,39 @@
 import requests
-from flask import Flask
+from flask import Flask, render_template, Markup, request, redirect
+
 
 app = Flask(__name__)
 API_KEY = '563492ad6f91700001000001b468a1cff0e34319b5287f382e03b2e1'
+default_search = 'Elephant'
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
+    search_query = request.form.get('search') if request.method=='POST' else default_search
+    return redirect(f'/search/{search_query}')
+    
+
+@app.route('/search/<search_query>')
+def search(search_query):
     endpoint = 'search'
     res = requests.get(
         f'https://api.pexels.com/v1/{endpoint}', 
-        params={'query': 'elephant'}, 
+        params={'query': search_query}, 
         headers={'Authorization': API_KEY}
     )
 
-    if res.ok:
+    try:
         res_json = res.json()
         img_type = 'medium'
-        urls = [img['src'][img_type] for img in res_json['photos']]
-        html = ""
-        for url in urls:
-            html += f'<div class="img_result"><a href="{url}" target="_blank">{url}</a></div>'
-        return html
-    else:
-        print(res)
-        return "Error"
+        img_urls = [{'url':img['url'], 'img':img['src'][img_type]} for img in res_json['photos']]
+        results_html = ""
+        for data in img_urls:
+            url = data['url']
+            img = data['img']
+            results_html += f'<div class="search_image"><a href="{url}" target="_blank"><img src="{img}"></a><div>'
+        return render_template("index.html", results = Markup(results_html))
+    except:
+        return "An Error Occured"
 
 
 app.run(debug=True)
