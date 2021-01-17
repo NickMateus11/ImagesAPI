df0a5b27cfbd76584a83db438219ed8cc6c7dc47
NickMateus11 <mateus.nicholas@gmail.com>
2021-01-17 13:30:21 -0500

    return img links

--- flask_image_repo.py
+++ flask_image_repo.py
@@ -1,6 +1,5 @@
 import requests
 from flask import Flask
-import json
 
 app = Flask(__name__)
 API_KEY = '563492ad6f91700001000001b468a1cff0e34319b5287f382e03b2e1'
@@ -16,7 +15,13 @@ def home():
     )
 
     if res.ok:
-        return res.json()
+        res_json = res.json()
+        img_type = 'medium'
+        urls = [img['src'][img_type] for img in res_json['photos']]
+        html = ""
+        for url in urls:
+            html += f'<div class="img_result"><a href="{url}" target="_blank">{url}</a></div>'
+        return html
     else:
         print(res)
         return "Error"
